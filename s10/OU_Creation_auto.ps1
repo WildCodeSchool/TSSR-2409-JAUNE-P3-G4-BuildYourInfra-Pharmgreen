09abc70eab41b68945146899008c5c5a8c01b420
pa-jovet <pa.jovet1@gmail.com>
2024-11-28 14:30:50 +0100

    Modif Scripts OU + Ajout Script User

--- s10/OU_Creation_auto.ps1
+++ s10/OU_Creation_auto.ps1
@@ -5,21 +5,21 @@
 #                                                                                                  #
 ####################################################################################################
 
-### Parametre(s) à modifier
-
-$File = "C:\Users\Administrator\Documents\OU_Creation_auto.txt"
-
 ### Initialisation
 
 $FilePath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 $DomainDN = (Get-ADDomain).DistinguishedName
+
+### Parametre(s) à modifier
+
+$File = "$FilePath\OU_Creation_auto.txt"
 
 ### Main
 
 function CreateOU
 {
    param ([Parameter(Mandatory=$True, Position=0)][String]$OU,
-            [Parameter(Mandatory=$True, Position=1)][ValidateSet("OU PgUsers","OU PgUsers/Communication","OU PgUsers/DirectionFinanciere","OU PgUsers/DirectionMarketing","OU PgUsers/R&D","OU PgUsers/RH","OU PgUsers/ServiceJuridique","OU PgUsers/ServicesGeneraux","OU PgUsers/SystemesInformation","OU PgUsers/VentesDeveloppementCommercial")][String]$SearchBase)
+            [Parameter(Mandatory=$True, Position=1)][ValidateSet("OU PgUsers","OU PgUsers/Communication","OU PgUsers/DirectionFinanciere","OU PgUsers/DirectionMarketing","OU PgUsers/R&D","OU PgUsers/RH","OU PgUsers/Juridique","OU PgUsers/SG","OU PgUsers/SystemesInformation","OU PgUsers/VDC")][String]$SearchBase)
     
     Switch ($SearchBase)
     {
@@ -29,10 +29,10 @@ function CreateOU
         "OU PgUsers/DirectionMarketing" {$DNSearchBase = "OU=DirectionMarketing,OU=PgUsers,$DomainDN"}
         "OU PgUsers/R&D" {$DNSearchBase = "OU=R&D,OU=PgUsers,$DomainDN"}
         "OU PgUsers/RH" {$DNSearchBase = "OU=RH,OU=PgUsers,$DomainDN"}
-        "OU PgUsers/ServiceJuridique" {$DNSearchBase = "OU=ServiceJuridique,OU=PgUsers,$DomainDN"}
-        "OU PgUsers/ServicesGeneraux" {$DNSearchBase = "OU=ServicesGeneraux,OU=PgUsers,$DomainDN"}
+        "OU PgUsers/Juridique" {$DNSearchBase = "OU=Juridique,OU=PgUsers,$DomainDN"}
+        "OU PgUsers/SG" {$DNSearchBase = "OU=SG,OU=PgUsers,$DomainDN"}
         "OU PgUsers/SystemesInformation" {$DNSearchBase = "OU=SystemesInformation,OU=PgUsers,$DomainDN"}
-        "OU PgUsers/VentesDeveloppementCommercial" {$DNSearchBase = "OU=VentesDeveloppementCommercial,OU=PgUsers,$DomainDN"}
+        "OU PgUsers/VDC" {$DNSearchBase = "OU=VDC,OU=PgUsers,$DomainDN"}
 
         default {$DNSearchBase = "OU=LabUtilisateurs,$DomainDN"}
     }
---
+++ s10/USER_Creation_auto.ps1
@@ -0,0 +1,66 @@
+﻿######################################################################################################
+#                                                                                                    #
+#   Création USER automatiquement avec fichier (avec suppression protection contre la suppression)   #
+#                                                                                                    #
+######################################################################################################
+
+$FilePath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
+$DomainDN = (Get-ADDomain).DistinguishedName
+
+### Parametre(s) à modifier
+
+$File = "$FilePath\s01_Pharmgreen - Liste Personnel.txt"
+
+### Main program
+
+Clear-Host
+If (-not(Get-Module -Name activedirectory))
+{
+    Import-Module activedirectory
+}
+
+# Civilite,Prenom,Nom,Societe,Site,Departement,Service,fonction,ManagerPrenom,ManagerNom,PC,DateNaissance,TelephoneFixe,TelephonePortable,Nomadisme
+
+$Users = Import-Csv -Path $File -Delimiter "," -Header "Civilite","Prenom","Nom","Societe","Site","Departement","Service","fonction","ManagerPrenom","ManagerNom","PC","DateNaissance","TelephoneFixe","TelephonePortable","Nomadisme"
+$ADUsers = Get-ADUser -Filter * -Properties *
+$count = 1
+Foreach ($User in $Users)
+{
+    Write-Progress -Activity "Création des utilisateurs dans l'OU" -Status "%effectué" -PercentComplete ($Count/$Users.Length*100)
+    $Name              = "$($User.nom) $($User.prenom)"
+    $DisplayName       = "$($User.prenom) $($User.nom)"
+    $SamAccountName    = "$($User.Prenom.tolower())" + "." + "$($User.Nom.ToLower())"
+    $UserPrincipalName = (($User.prenom.tolower() + "." + $User.nom.ToLower()) + "@" + (Get-ADDomain).Forest)
+    $GivenName         = $User.Prenom
+    $Surname           = $User.Nom
+    $OfficePhone       = $User.TelephoneFixe
+    $MobilePhone       = $User.TelephonePortable
+    $EmailAddress      = $UserPrincipalName
+    If ( $User.Service -eq "-" )
+        {
+        $Path              = "ou=$($User.Departement),ou=PgUsers,$DomainDN"
+        }
+    Else
+        {
+        $Path              = "ou=$($User.Service),ou=$($User.Departement),ou=PgUsers,$DomainDN"
+        }
+    $Company           = $User.Societe
+    $JobTitle          = $User.fonction
+    $Department        = "$($User.Departement) - $($User.Service)"
+    
+    If (($ADUsers | Where {$_.SamAccountName -eq $SamAccountName}) -eq $Null)
+    {
+        New-ADUser -Name $Name -DisplayName $DisplayName -SamAccountName $SamAccountName -UserPrincipalName $UserPrincipalName -Description $JobTitle `
+        -GivenName $GivenName -Surname $Surname -OfficePhone $OfficePhone -MobilePhone $MobilePhone -EmailAddress $EmailAddress -Title $JobTitle `
+        -Path $Path -AccountPassword (ConvertTo-SecureString -AsPlainText Azerty1* -Force) -Enabled $True `
+        -OtherAttributes @{Company = $Company;Department = $Department} -ChangePasswordAtLogon $True
+
+        Write-Host "Création du USER $SamAccountName" -ForegroundColor Green
+    }
+    Else
+    {
+        Write-Host "Le USER $SamAccountName existe déjà" -ForegroundColor Black -BackgroundColor Yellow
+    }
+    $Count++
+    sleep -Milliseconds 100
+}
