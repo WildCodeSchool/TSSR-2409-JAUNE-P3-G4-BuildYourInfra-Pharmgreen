c24dd08f30b73161df7e63653e2fcb17dd23a62b
pa-jovet <pa.jovet1@gmail.com>
2025-01-09 09:48:55 +0100

    Scripts S5

---
+++ Ressources/S5/DossiersIndiv-Deny-Deletion.ps1
@@ -0,0 +1,16 @@
+﻿################################################
+#                                              #
+#         Refuser le droit de supprimer        #
+#           les dossiers individuels           #
+#                                              #
+################################################
+
+# Récupération de la liste des dossiers individuels
+$RepList = Get-ChildItem '\\srvprod\Pharmgreen\00_Commun\002_Dossiers Individuels\'
+# Récupération du groupe des utilisateurs standards
+$GrpStandard = (Get-ADGroup -Filter * | ?{$_.name -match "grpuserspharmgreenstandards"}).Name
+
+# Boucle d'ajout d'une règle sur chaque dossier de la liste
+foreach ($RepIndiv in $RepList)
+{
+    $Path = "\\srvprod\Pharmgreen\00_Commun\002_Dossiers Individuels\$RepIndiv"    $ACL = Get-Acl -Path $Path    $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("$GrpStandard","Delete","Deny")    $ACL.SetAccessRule($AccessRule)    $ACL | Set-Acl -Path $Path    (Get-Acl -Path $Path).Access | Format-Table IdentityReference,FileSystemRights,AccessControlType,IsInherited,InheritanceFlags -AutoSize}
---
+++ Ressources/S5/GPO-MappingServices-Modif-SecurityFiltering_Status.ps1
@@ -0,0 +1,20 @@
+﻿################################################
+#                                              #
+#    Modification Security Filtering de GPO    #
+#          Modification GPO Status             #
+#                                              #
+################################################
+
+# Ajout des Domain Computers dans les Security Filtering
+get-gpo -all -Domain "pharmgreen.lan" | Where-Object {$_.DisplayName -match "user-mappage-service*"} | Set-GPPermissions -PermissionLevel None -TargetName "Authenticated Users" -TargetType Group
+# Retrait des Authenticated Users des Security Filtering
+get-gpo -all -Domain "pharmgreen.lan" | Where-Object {$_.DisplayName -match "user-mappage-service*"} | Set-GPPermissions -PermissionLevel GpoApply -TargetName "Domain Computers" -TargetType Group
+
+
+$GPOs = get-gpo -all -Domain "pharmgreen.lan" | Where-Object {$_.DisplayName -match "user-mappage-service*"}
+
+# Modification du GPO Status pour désactiver les paramètres Ordinateur
+foreach ($GPO in $GPOs)
+{
+    $GPO.GpoStatus="ComputerSettingsDisabled"
+}
